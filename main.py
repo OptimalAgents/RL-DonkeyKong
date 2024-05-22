667f4396270e3deec183eb0c2a63dcaa1580f505
szuvarska <01171269@pw.edu.pl>
2024-05-23 00:18:48 +0200

    Add ladder mask

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ TORCH_MASK[68:75, 40:42] = 1
 
 MARIO_COLOR = np.array((200, 72, 72))
 
+LADDER_MASK = np.zeros((210, 160))
+LADDER_MASK[160:196, 106:112] = 1  # First level
+LADDER_MASK[128:169, 79:84] = 1  # Second level
+LADDER_MASK[132:160, 47:53] = 1  # Second level
+LADDER_MASK[102:140, 87:92] = 1  # Third level
+LADDER_MASK[104:137, 108:112] = 1  # Third level
+LADDER_MASK[74:112, 67:72] = 1  # Fourth level
+LADDER_MASK[71:110, 48:52] = 1  # Fourth level
+LADDER_MASK[45:82, 107:112] = 1  # Fifth level
+LADDER_MASK[20:60, 76:80] = 1  # Sixth level
+LADDER_MASK[20:60, 32:36] = 1  # Sixth level
+
 
 def find_barrels(observation: np.ndarray) -> list[tuple[int, int]]:
     barrels = []
@@ -38,10 +50,7 @@ def find_mario(observation: np.ndarray) -> tuple[int, int]:
 
 def ladder_close(observation: np.ndarray) -> bool:
     mario_coords = find_mario(observation)
-    ladder_mask = np.zeros((210, 160))
-    ladder_mask[160:196, 106:112] = 1
-    # ladder_mask[144:169, 79:84] = 1
-    if ladder_mask[mario_coords[0], mario_coords[1]] == 1:
+    if LADDER_MASK[mario_coords[0], mario_coords[1]] == 1:
         return True
     return False
 
@@ -49,7 +58,7 @@ def ladder_close(observation: np.ndarray) -> bool:
 def main():
     env = gym.make("ALE/DonkeyKong-v5", render_mode="human")
     observation, info = env.reset()
-    env.env.ale.saveScreenPNG(b'test_image.png')  # save screenshot
+    # env.env.ale.saveScreenPNG(b'test_image.png')  # save screenshot
     observation, reward, terminated, truncated, info = env.step(1)
     climbing = False
     climbed_ladders = 0
@@ -58,14 +67,15 @@ def main():
             action = 10
             climbing = True
         else:
-            if climbed_ladders%2 == 0:
-                action = 3
-            else:
-                action = 4
             if climbing:
                 climbed_ladders += 1
                 climbing = False
-        print(action)
+            if climbed_ladders % 2 == 0:
+                action = 3
+            else:
+                action = 4
+        print(i)
+        print(climbed_ladders)
         observation, reward, terminated, truncated, info = env.step(action)
 
     # for i in range(1000):
